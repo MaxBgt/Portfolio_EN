40f3ac6748b4a9b4fcd28b2f87eff2813053decf
Maxime Bignolet <maxime.bignolet@gmail.com>
2023-04-27 08:19:17 +0200

    3D carousel

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,9 +2,16 @@ import Card from "@/components/Card";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const portfolio = () => {
   const [projects, setProjects] = useState([]);
+  const [imageIndex, setImageIndex] = useState(0);
   useEffect(() => {
     axios
       .get("/api/projects")
@@ -13,6 +20,20 @@ const portfolio = () => {
         console.error("Erreur lors de la récupération des projets:", error)
       );
   }, []);
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FontAwesomeIcon icon={faChevronRight} />
+      </div>
+    );
+  };
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </div>
+    );
+  };
   return (
     <AnimatePresence>
       <motion.div
@@ -29,9 +50,26 @@ const portfolio = () => {
           </h3>
         </div>
         <div className="card_container">
-          {projects.map((project) => (
-            <Card key={project.id} project={project} />
-          ))}
+          <Slider
+            infinite={true}
+            lazyLoad={true}
+            speed={300}
+            slidesToShow={3}
+            centerMode={true}
+            centerPadding={0}
+            nextArrow={<NextArrow />}
+            prevArrow={<PrevArrow />}
+            beforeChange={(current, next) => setImageIndex(next)}
+          >
+            {projects.map((project, idx) => (
+              <div
+                className={idx === imageIndex ? "slide activeSlide" : "slide"}
+                key={idx}
+              >
+                <Card key={project.id} project={project} />
+              </div>
+            ))}
+          </Slider>
         </div>
       </motion.div>
     </AnimatePresence>
